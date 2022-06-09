81545bdafe5f836b51a16d6075e69f3c62e23578
PortalMaster555 <portalmaster55@gmail.com>
2022-06-08 23:52:15 -0500

    user input taken successfully

--- lettercount.c
+++ lettercount.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
+char word[32];
+int sum;
 int main(int argc, char **argv)
 {
-	printf("Hello, world!%c", '\n');
+	printf("Please enter a word to obtain a letter count: ");
+	fgets(word, sizeof(word), stdin);
+	word[strlen(word) - 1] = '\0'; //hacky way to remove pesky newlines
+	printf("The word you chose is %s, which has a letter sum of %d.\n", word, sum);
+	printf("Press ENTER to continue");
+	getchar();
 	return 0;
 }
