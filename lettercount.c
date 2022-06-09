3d3e94b98bb45955e27575ef50146c3097cb0f3c
PortalMaster555 <portalmaster55@gmail.com>
2022-06-09 00:12:13 -0500

    Functioning letter value counter

--- lettercount.c
+++ lettercount.c
@@ -1,15 +1,29 @@
 #include<stdio.h>
 #include<string.h>
+#include <ctype.h>
 
 char word[32];
-int sum;
+int sum = 0;
+
 int main(int argc, char **argv)
 {
 	printf("Please enter a word to obtain a letter count: ");
+	
 	fgets(word, sizeof(word), stdin);
+	
+	for(int i = 0; i < strlen(word) - 1; i++)
+	{
+		printf("%c->", toupper(word[i])); 
+		word[i] = tolower(word[i]);
+		printf("%d ", word[i] - 96); //Offsetting ASCII value to A1Z26 
+		sum += word[i] - 96; //more magic numbers
+	}
+
 	word[strlen(word) - 1] = '\0'; //hacky way to remove pesky newlines
-	printf("The word you chose is %s, which has a letter sum of %d.\n", word, sum);
-	printf("Press ENTER to continue");
+	printf("%c", '\n');
+	printf("The word you chose is %s, which has a letter sum of %d.%c", word, sum, '\n');
+	
+	printf("Press ENTER to continue. ");
 	getchar();
 	return 0;
 }
